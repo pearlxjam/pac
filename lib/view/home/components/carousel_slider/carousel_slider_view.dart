efb2989c21032c6c5d74da8fb26870efd248ba62
rodrigo.morales.soto@gmail.com <Monosxesk8>
2024-08-28 16:21:43 -0400

    cambio en el carousel

--- lib/view/home/components/carousel_slider/carousel_slider_view.dart
+++ lib/view/home/components/carousel_slider/carousel_slider_view.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:pac/model/modelos.dart';
 import 'package:pac/view/view.dart';
